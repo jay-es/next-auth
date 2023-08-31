f79ce7bf3f7f9d255bb9b7851a1fa93d1165cf00
Jun Shindo <46585162+jay-es@users.noreply.github.com>
2023-08-31 18:28:38 +0900

    props の型定義

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -2,11 +2,11 @@ import Link from 'next/link';
 
 import { AuthButtons } from './_components/AuthButtons';
 
-export default function HeaderLayout({
-  children,
-}: {
+type Props = {
   children: React.ReactNode;
-}) {
+};
+
+export default function HeaderLayout({ children }: Props) {
   return (
     <>
       <header className="flex justify-between py-4">
